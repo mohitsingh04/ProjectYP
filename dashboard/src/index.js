013cc575bb2777928e9b5a869477ea2f29462163
TanishkKumar0409 <tanishkk60@gmail.com>
2025-03-24 15:49:26 +0530

    Remove redux

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import store from "./redux/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
